a0148b3f7433a9282f3404de9f4da2d5d3691345
daisysouza <daisysouza22@gmail.com>
2022-11-28 20:26:48 -0300

    aula2

---
+++ 02-condicionais/aula2.js
@@ -0,0 +1,18 @@
+
+const numero = 5;
+
+const isNumeroPar = (numero % 2) === 0;
+
+if (isNumeroPar) {
+    console.log('O número é par');
+} else {
+    console.log('o número é impar');
+}
+
+// Pega o numero e divide pelo resto da divisão: %
+
+// Para saber se o número é par o resto da divisão deve ser maior que 0
+
+// Sempre o resultado de uma condicional é um booleano (expressão que retorna um booleano)
+
+// !numeroPar - negação
